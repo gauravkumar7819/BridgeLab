791de72d4f1f491609504c677139bdfa51f65a99
Gaurav Kumar <122110711+gauravkumar7819@users.noreply.github.com>
2025-06-17 10:01:33 +0530

    added solution for YoungestAndTallestFARIEND

---
+++ ArrayPrograms/YoungestAndTallestFriend.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class YoungestAndTallestFriend {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+
+            System.out.println("Enter height (in cm) of " + names[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        System.out.println("The youngest friend is: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+        System.out.println("The tallest friend is: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+
+        sc.close();
+    }
+}
