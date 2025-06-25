3cddf31df62d7df9a9030c82030c2917bd4729a7
Gaurav Kumar <122110711+gauravkumar7819@users.noreply.github.com>
2025-06-25 11:43:57 +0530

    file changed

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // This is the entry point of the application
-        System.out.println("Hello, BridgeLab!");
-        
-        // You can add more functionality here
-        // For example, you can create instances of other classes or call methods
-    }
-}
