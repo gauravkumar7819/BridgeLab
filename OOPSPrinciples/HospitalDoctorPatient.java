9542f3b481e00b9e52f27dbef958d4ac78819779
Gaurav Kumar <122110711+gauravkumar7819@users.noreply.github.com>
2025-06-28 00:01:11 +0530

    added solution for HospitalDoctorPatient

--- OOPSPrinciples/HospitalDoctorPatient.java
+++ OOPSPrinciples/HospitalDoctorPatient.java
@@ -1,40 +1,38 @@
 import java.util.*;
 
 class Patient {
-    private String name;
+    private String nam;
     private List<Doctor> doctors = new ArrayList<>();
-    public Patient(String name) { this.name = name; }
-    public String getName() { return name; }
+    public Patient(String nam) { this.nam = nam; }
+    public String getnam() { return nam; }
     public void addDoctor(Doctor doctor) {
         if (!doctors.contains(doctor)) doctors.add(doctor);
     }
     public void showDoctors() {
-        System.out.println(name + " consulted:");
-        for (Doctor d : doctors) System.out.println("- " + d.getName());
+        System.out.println(nam + " consulted:");
+        for (Doctor d : doctors) System.out.println("- " + d.getnam());
     }
 }
-
 class Doctor {
-    private String name;
+    private String nam;
     private List<Patient> patients = new ArrayList<>();
-    public Doctor(String name) { this.name = name; }
-    public String getName() { return name; }
+    public Doctor(String nam) { this.nam = nam; }
+    public String getnam() { return nam; }
     public void consult(Patient patient) {
         if (!patients.contains(patient)) patients.add(patient);
         patient.addDoctor(this);
-        System.out.println("Dr. " + name + " consulted " + patient.getName());
+        System.out.println("Dr. " + nam + " consulted " + patient.getnam());
     }
     public void showPatients() {
-        System.out.println("Dr. " + name + " has patients:");
-        for (Patient p : patients) System.out.println("- " + p.getName());
+        System.out.println("Dr. " + nam + " has patients:");
+        for (Patient p : patients) System.out.println("- " + p.getnam());
     }
 }
 
 class Hospital {
-    private String name;
-    public Hospital(String name) { this.name = name; }
+    private String nam;
+    public Hospital(String nam) { this.nam = nam; }
 }
-
 public class HospitalDoctorPatient {
     public static void main(String[] args) {
         Doctor drA = new Doctor("Alice");
