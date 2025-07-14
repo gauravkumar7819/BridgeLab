068b94cad8a50319a4eb8d1d5b489afc6d041be4
Gaurav Kumar <122110711+gauravkumar7819@users.noreply.github.com>
2025-07-14 12:19:43 +0530

    added solution for ZeroSumSubarrays

---
+++ Stack_QueueAndHashMap/ZeroSumSubarrays.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ZeroSumSubarrays {
+    public static void findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<int[]> result = new ArrayList<>();
+        int sum = 0;
+        map.put(0, new ArrayList<>());
+        map.get(0).add(-1);
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum)) {
+                for (int start : map.get(sum)) {
+                    result.add(new int[]{start + 1, i});
+                }
+            }
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+        for (int[] sub : result) {
+            for (int i = sub[0]; i <= sub[1]; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        findZeroSumSubarrays(arr);
+    }
+}
+
