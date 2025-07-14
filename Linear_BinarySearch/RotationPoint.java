13d8ed55999a1db66e1798365e9137604fa9f4d5
Gaurav Kumar <122110711+gauravkumar7819@users.noreply.github.com>
2025-07-14 09:53:14 +0530

    added solution for RotationPoint

---
+++ Linear_BinarySearch/RotationPoint.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RotationPoint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(scanner.nextLine());
+        }
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        System.out.println(left);
+    }
+}
+
