0ee97d8a0e654734b27d86e352ea3bea90d50161
Gaurav Kumar <122110711+gauravkumar7819@users.noreply.github.com>
2025-07-14 12:19:28 +0530

    added solution for TwoSum

---
+++ Stack_QueueAndHashMap/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] findTwoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int[] result = findTwoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
